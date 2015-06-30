24cbe08d4f80605ea5e3eaac1d7907b30b216060
Robert Jordens <jordens@gmail.com>
2015-06-29 23:19:57 -0600

    cleanup and simplify
    shift is changed on falling clock edges which fits fine. no synchronous logic
    required, latency is one clock cycle

--- xilinx.py
+++ xilinx.py
@@ -2,58 +2,54 @@
 # Robert Jordens <jordens@gmail.com> 2015
 
 from migen.fhdl.std import *
-from migen.genlib.record import Record
 from mibuild.generic_platform import ConstraintError
 
-from bscan_spi import bscan_layout, BscanSpi
+
+bscan_layout = [
+    ("tdi", 1),
+    ("tdo", 1),
+    ("tck", 1),
+    ("sel", 1),
+    ("shift", 1),
+]
 
 
 class Spartan6(Module):
     def __init__(self, platform):
-        bscan = Record(bscan_layout)
         spi = platform.request("spiflash")
-        self.submodules.bscan2spi = BscanSpi(bscan, spi)
-        self.specials += Instance(
-            "BSCAN_SPARTAN6", p_JTAG_CHAIN=1,
-            o_CAPTURE=bscan.capture, o_DRCK=bscan.drck,
-            o_RESET=bscan.rst, o_SEL=bscan.sel, o_UPDATE=bscan.update,
-            o_TDI=bscan.tdi, i_TDO=bscan.tdo)
+        shift = Signal()
+        self.specials += Instance("BSCAN_SPARTAN6", p_JTAG_CHAIN=1,
+                                  o_TCK=spi.clk, o_SHIFT=shift,
+                                  o_TDI=spi.mosi, i_TDO=spi.miso)
+        self.comb += spi.cs_n.eq(~shift)
         try:
-            self.comb += platform.request("user_led").eq(~spi.cs_n)
-            self.comb += platform.request("user_led").eq(1)
+            self.comb += platform.request("user_led", 0).eq(1)
+            self.comb += platform.request("user_led", 1).eq(shift)
         except ConstraintError:
             pass
 
 
 class Series7(Module):
     def __init__(self, platform):
-        bscan = Record(bscan_layout)
-        spi4 = platform.request("spiflash")
-        spi = Record([("clk", 1), ("miso", 1), ("mosi", 1), ("cs_n", 1)])
-        self.comb += [
-            spi4.dq[0].eq(spi.mosi),
-            spi.miso.eq(spi4.dq[1]),
-            spi4.cs_n.eq(spi.cs_n),
-            # spi.clk == usrcclko == bscan.drck
-        ]
-        self.submodules.bscan2spi = BscanSpi(bscan, spi)
-        self.specials += Instance(
-            "BSCANE2", p_JTAG_CHAIN=1,
-            o_CAPTURE=bscan.capture, o_DRCK=bscan.drck,
-            o_RESET=bscan.rst, o_SEL=bscan.sel, o_UPDATE=bscan.update,
-            o_TDI=bscan.tdi, i_TDO=bscan.tdo)
-        self.specials += Instance(
-            "STARTUPE2", i_CLK=0, i_GSR=0, i_GTS=0, i_KEYCLEARB=0,
-            i_PACK=1, i_USRCCLKO=bscan.drck, i_USRCCLKTS=0, i_USRDONEO=1,
-            i_USRDONETS=1)
+        spi = platform.request("spiflash")
+        shift = Signal()
+        clk = Signal()
+        self.comb += spi.cs_n.eq(~shift)
+        self.specials += Instance("BSCANE2", p_JTAG_CHAIN=1,
+                                  o_SHIFT=shift, o_TCK=clk,
+                                  io_TDI=spi.dq[0], io_TDO=spi.dq[1])
+        self.specials += Instance("STARTUPE2", i_CLK=0, i_GSR=0, i_GTS=0,
+                                  i_KEYCLEARB=0, i_PACK=1, i_USRCCLKO=clk,
+                                  i_USRCCLKTS=0, i_USRDONEO=1, i_USRDONETS=1)
         try:
-            self.comb += platform.request("user_led").eq(~spi.cs_n)
-            self.comb += platform.request("user_led").eq(1)
+            self.comb += platform.request("user_led", 0).eq(1)
+            self.comb += platform.request("user_led", 1).eq(shift)
         except ConstraintError:
             pass
 
 
 def build_bscan_spi(platform, Top):
+    platform.toolchain.bitgen_opt += " -g compress"
     name = "bscan_spi_{}".format(platform.device)
     top = Top(platform)
     platform.build_cmdline(top, build_name=name)
@@ -63,13 +59,10 @@ if __name__ == "__main__":
     from mibuild.platforms import pipistrello, papilio_pro, kc705
 
     p = pipistrello.Platform()
-    p.toolchain.bitgen_opt += " -g compress"
     build_bscan_spi(p, Spartan6)
 
     p = papilio_pro.Platform()
-    p.toolchain.bitgen_opt += " -g compress"
     build_bscan_spi(p, Spartan6)
 
     p = kc705.Platform(toolchain="ise")
-    p.toolchain.bitgen_opt += " -g compress"
     build_bscan_spi(p, Series7)
